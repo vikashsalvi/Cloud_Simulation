28b274264628e23e8732114fb586f2f7fe426188
Vikash Manharlal Salvi <vk299944@dal.ca>
2020-03-25 15:54:01 -0300

    Added files

---
+++ tests/test/unit.test.js
@@ -0,0 +1,33 @@
+const expect = require('chai').expect;
+
+const searchResponse = require('/search/hello');
+
+describe('Get simple search result', () => {
+  beforeEach(() => {
+    nock('http://ec2-52-90-72-138.compute-1.amazonaws.com:8080/search/hello')
+      .get('/search/hello')
+      .reply(200, response);
+  });
+
+  it('Get a search response by username', () => {
+    return searchResponse.then(response => {
+      expect(response).to.not.equal('')
+      });
+  });
+});
+
+const searchResponse = require('/search/hello');
+
+describe('Get simple search result', () => {
+  beforeEach(() => {
+    nock('http://ec2-52-90-72-138.compute-1.amazonaws.com:8080/search/hello')
+      .get('/search/hello')
+      .reply(200, response);
+  });
+
+  it('Get a search response by username', () => {
+    return searchResponse.then(response => {
+      expect(response).to.not.equal('')
+      });
+  });
+});
